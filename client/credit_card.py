d7376cf2d0f99d7a877f231b007f2a77fc359206
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-10-29 15:13:44 -0300

    add test get brand

--- client/credit_card.py
+++ client/credit_card.py
@@ -1,11 +1,43 @@
+import re
 from .utils.encoders import ObjectJsonSerializable
+
+BRAND_VISA = 'visa'
+BRAND_MASTERCARD = 'mastercard'
+BRAND_AMEX = 'american express'
+BRAND_DISCOVER = 'discover'
+BRAND_DANKORT = 'dankort'
+BRAND_MAESTRO = 'maestro'
+BRAND_DINERS = 'diners club'
+
+BRANDS = {
+    BRAND_VISA: re.compile(r'^4\d{12}(\d{3})?$'),
+    BRAND_MASTERCARD: re.compile(r'''
+        ^(5[1-5]\d{4}|677189)\d{10}$|  
+        ^(222[1-9]|2[3-6]\d{2}|27[0-1]\d|2720)\d{12}$   
+    ''', re.VERBOSE),
+    BRAND_AMEX: re.compile(r'^3[47]\d{13}$'),
+    BRAND_DISCOVER: re.compile(r'^(6011|65\d{2})\d{12}$'),
+    BRAND_DANKORT: re.compile(r'^(5019)\d{12}$'),
+    BRAND_MAESTRO:
+        re.compile(r'^(?:5[0678]\d\d|6304|6390|67\d\d)\d{8,15}$'),
+    BRAND_DINERS:
+        re.compile(r'^3(?:0[0-5]|[68][0-9])[0-9]{11}$'),
+}
 
 
 class CreditCard(ObjectJsonSerializable):
     def __init__(self, cvv, brand):
-        self.card_number = None
+        self.number = None
         self.holder = None
         self.expiryMonth = None
         self.expiryYear = None
         self.cvv = cvv
         self.brand = brand
+
+    @staticmethod
+    def get_brand(number):
+        number = number.replace(' ', '')
+        for brand, regexp in BRANDS.items():
+            if regexp.match(number):
+                return brand
+        return None
--- client/tests/test_credit_card.py
+++ client/tests/test_credit_card.py
@@ -4,7 +4,7 @@ from client.credit_card import CreditCard
 
 card_data = {
     "brand": "visa",
-    "card_number": None,
+    "number": None,
     "cvv": "123",
     "expiryMonth": None,
     "expiryYear": None,
@@ -12,10 +12,19 @@ card_data = {
 }
 
 
-def test_card_json():
+def test_create_card_json():
     card = CreditCard('123', 'visa')
     assert isinstance(card, CreditCard)
     assert card.toJSON() == json.dumps(card_data,
                                        sort_keys=True, indent=4)
     assert card.cvv == card_data.get('cvv')
     assert card.brand == card_data.get('brand')
+
+
+def test_get_brand():
+    assert 'visa' == CreditCard.get_brand("4761 1200 0000 0148")
+    assert 'mastercard' == CreditCard.get_brand("5168 4412 2363 0339")
+    assert 'maestro' == CreditCard.get_brand("5080 7415 4958 8144 561")
+    assert 'american express' == CreditCard.get_brand("3716 4219 0784 801")
+    
+    assert not CreditCard.get_brand('')
