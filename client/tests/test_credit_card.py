c997956f9ce3217a94fe325e283bc74f2ac7cc5f
Gabriela Cavalcante da Silva <gabicavalcantesilva@gmail.com>
2019-10-30 15:22:12 -0300

    remove null attrs before make json dumps

--- client/tests/test_credit_card.py
+++ client/tests/test_credit_card.py
@@ -3,27 +3,24 @@ from client.credit_card import CreditCard
 
 card_data = {
     "brand": "visa",
-    "number": None,
     "cvv": "123",
-    "expiryMonth": None,
-    "expiryYear": None,
-    "holder": None
 }
 
 
 def test_create_card_json():
-    card = CreditCard('123', 'visa')
+    card = CreditCard()
+    card.cvv = "123"
+    card.brand = "visa"
     assert isinstance(card, CreditCard)
-    assert card.toJSON() == json.dumps(card_data,
-                                       sort_keys=True, indent=4)
-    assert card.cvv == card_data.get('cvv')
-    assert card.brand == card_data.get('brand')
+    assert card.toJSON() == json.dumps(card_data, sort_keys=True, indent=4)
+    assert card.cvv == card_data.get("cvv")
+    assert card.brand == card_data.get("brand")
 
 
 def test_get_brand():
-    assert 'visa' == CreditCard.get_brand("4761 1200 0000 0148")
-    assert 'mastercard' == CreditCard.get_brand("5168 4412 2363 0339")
-    assert 'maestro' == CreditCard.get_brand("5080 7415 4958 8144 561")
-    assert 'american express' == CreditCard.get_brand("3716 4219 0784 801")
+    assert "visa" == CreditCard.get_brand("4761 1200 0000 0148")
+    assert "mastercard" == CreditCard.get_brand("5168 4412 2363 0339")
+    assert "maestro" == CreditCard.get_brand("5080 7415 4958 8144 561")
+    assert "american express" == CreditCard.get_brand("3716 4219 0784 801")
 
-    assert not CreditCard.get_brand('')
+    assert not CreditCard.get_brand("")
--- client/utils/encoders.py
+++ client/utils/encoders.py
@@ -1,8 +1,18 @@
 import json
+from datetime import datetime, date
 
 
 class ObjectJsonSerializable(object):
     def toJSON(self):
-        # TODO: remove null field
-        return json.dumps(self, default=lambda o: o.__dict__,
-                          sort_keys=True, indent=4)
+        remove_null(self.__dict__)
+
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4,)
+
+
+def remove_null(d):
+    for key, value in list(d.items()):
+        if value is None:
+            del d[key]
+        elif isinstance(value, dict):
+            del_none(value)
+    return d
